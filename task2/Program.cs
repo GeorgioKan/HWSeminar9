41ceefe5b977585ecd2cb8e1a6c0edc5c57e7091
Gerorge <logovas4@gmail.com>
2023-02-13 12:38:22 +0300

    Task2 added

---
+++ task2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число 
+// и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+
+
+int Function(int numb)
+{
+    if (numb == 0)
+    {
+        return 0;
+    }
+    return numb%10 + Function(numb/10);    
+}
+
+int number = 123;
+System.Console.WriteLine(Function(number));
